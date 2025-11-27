e9d654fb63fe96628c2afb834b899289a5e056ca
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-11-27 10:14:16 -0300

    build: Ajusta as dependências para tulizar o TypeORM e Postgres

--- server/entities/Produto.ts
+++ server/entities/Produto.ts
@@ -8,4 +8,48 @@ class Produto{
     private _preco : number;
     private _categoria : Categoria;
 
+
+    get id(): number {
+        return this._id;
+    }
+
+    set id(value: number) {
+        this._id = value;
+    }
+
+    get nome(): string {
+        return this._nome;
+    }
+
+    set nome(value: string) {
+        this._nome = value;
+    }
+
+    get descricao(): string {
+        return this._descricao;
+    }
+
+    set descricao(value: string) {
+        this._descricao = value;
+    }
+
+    get preco(): number {
+        return this._preco;
+    }
+
+    set preco(value: number) {
+        this._preco = value;
+    }
+
+    get categoria(): Categoria {
+        return this._categoria;
+    }
+
+    set categoria(value: Categoria) {
+        this._categoria = value;
+    }
+
+
+    constructor() {
+    }
 }
