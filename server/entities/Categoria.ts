8749c938c22876975e13cecd8dba0e7c4c4b8c36
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-11-27 08:27:24 -0300

    feat: adiciona classes iniciais de dominio

---
+++ server/entities/Categoria.ts
@@ -0,0 +1,25 @@
+
+export class Categoria{
+
+    private _id : number;
+    private _descricao : string;
+
+
+    get id(): number {
+        return this._id;
+    }
+
+    set id(value: number) {
+        this._id = value;
+    }
+
+    get descricao(): string{
+        return this._descricao
+    }
+
+    set descricao(descricao : string) {
+        this._descricao = descricao
+    }
+
+
+}
---
+++ server/entities/Produto.ts
@@ -0,0 +1,11 @@
+import { Categoria} from "./Categoria";
+
+class Produto{
+
+    private _id : number;
+    private _nome : string;
+    private _descricao : string;
+    private _preco : number;
+    private _categoria : Categoria;
+
+}
